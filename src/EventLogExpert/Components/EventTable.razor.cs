d36fd06e03746c31e57e2e6542ab48f0a18194a7
Bill Long <bilong@microsoft.com>
2022-10-25 01:10:40 -0500

    Fix weird stretchy table issue

--- src/EventLogExpert/Components/EventTable.razor.cs
+++ src/EventLogExpert/Components/EventTable.razor.cs
@@ -7,13 +7,27 @@ public partial class EventTable
 {
     private readonly Dictionary<string, int> _colWidths = new()
     {
-        { "RecordId", 10 },
-        { "TimeCreated", 25 },
-        { "Id", 10 },
-        { "MachineName", 10 },
-        { "Level", 15 },
-        { "ProviderName", 25 },
-        { "Task", 20 },
-        { "Description", 200 }
+        { "RecordId", 75 },
+        { "TimeCreated", 165 },
+        { "Id", 50 },
+        { "MachineName", 100 },
+        { "Level", 100 },
+        { "ProviderName", 250 },
+        { "Task", 150 }
     };
+
+    private const int TableDividerWidth = 4;
+
+    private const int ScrollBarWidth = 18;
+
+    private string GetDescriptionStyle()
+    {
+        var total = _colWidths.Values.Sum() + (TableDividerWidth * _colWidths.Count) + ScrollBarWidth;
+        return $"min-width: calc(100vw - {total}px); max-width: calc(100vw - {total}px);";
+    }
+
+    private string GetInlineStyle(string colName)
+    {
+        return $"min-width: {_colWidths[colName]}px; max-width: {_colWidths[colName]}px;";
+    }
 }
