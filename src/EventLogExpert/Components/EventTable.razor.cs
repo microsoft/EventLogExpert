37603f1a0302f3151df8dc2df54cf8f78dfd84a8
Bill Long <bilong@microsoft.com>
2022-10-25 20:44:20 -0500

    Implement column resizing

--- src/EventLogExpert/Components/EventTable.razor.cs
+++ src/EventLogExpert/Components/EventTable.razor.cs
@@ -1,6 +1,10 @@
 // // Copyright (c) Microsoft Corporation.
 // // Licensed under the MIT License.
 
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using System.Diagnostics;
+
 namespace EventLogExpert.Components;
 
 public partial class EventTable
@@ -20,6 +24,20 @@ public partial class EventTable
 
     private const int ScrollBarWidth = 18;
 
+    private string _columnResizingName = "";
+
+    private double _columnMouseXLast = -1;
+
+    private IJSObjectReference? _jsModule;
+
+    private DotNetObjectReference<EventTable>? _thisObj;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        _jsModule = await JS.InvokeAsync<IJSObjectReference>("import", "./Components/EventTable.razor.js");
+        _thisObj = DotNetObjectReference.Create(this);
+    }
+
     private string GetDescriptionStyle()
     {
         var total = _colWidths.Values.Sum() + (TableDividerWidth * _colWidths.Count) + ScrollBarWidth;
@@ -29,5 +47,31 @@ public partial class EventTable
     private string GetInlineStyle(string colName)
     {
         return $"min-width: {_colWidths[colName]}px; max-width: {_colWidths[colName]}px;";
+    }
+
+    private void OnMouseDownDivider(MouseEventArgs e, string columnName)
+    {
+        _columnResizingName = columnName;
+        _columnMouseXLast = e.ClientX;
+        _jsModule?.InvokeVoidAsync("startColumnResize", new[] {_thisObj});
+    }
+
+    [JSInvokable]
+    public void MouseMoveCallback(MouseEventArgs e)
+    {
+        int difference = (int)(_columnMouseXLast - e.ClientX);
+        var newColumnWidth = _colWidths[_columnResizingName] - difference;
+        if (newColumnWidth < 10) newColumnWidth = 10;
+
+        _colWidths[_columnResizingName] = newColumnWidth;
+        _columnMouseXLast = e.ClientX;
+        StateHasChanged();
+    }
+
+    [JSInvokable]
+    public void MouseUpCallback()
+    {
+        _columnResizingName = "";
+        _columnMouseXLast = -1;
     }
 }
---
+++ src/EventLogExpert/Components/EventTable.razor.js
@@ -0,0 +1,13 @@
+﻿export function startColumnResize(dotNetObj) {
+    document.onmousemove = e => {
+        // For some reason we have to manually pass this as a new object. Simply passing the event
+        // to .NET does not work. We're only including the one value we care about.
+        dotNetObj.invokeMethodAsync('MouseMoveCallback', { ClientX: e.clientX });
+    };
+
+    document.onmouseup = e => {
+        document.onmousemove = null;
+        document.onmouseup = null;
+        dotNetObj.invokeMethodAsync('MouseUpCallback');
+    };
+}
