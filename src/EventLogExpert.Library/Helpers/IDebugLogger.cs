22303deee10a7b414cf601a0282ec99dc2aea0e4
Bill Long <bilong@microsoft.com>
2023-01-24 15:59:01 -0600

    Add some basic tracing

---
+++ src/EventLogExpert.Library/Helpers/IDebugLogger.cs
@@ -0,0 +1,24 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+namespace EventLogExpert.Library.Helpers;
+
+public interface ITraceLogger
+{
+    void Trace(string message);
+}
+
+public class DebugLogger : ITraceLogger
+{
+    private Action<string> _tracer;
+
+    public DebugLogger(Action<string> tracer)
+    {
+        _tracer = tracer;
+    }
+
+    public void Trace(string message)
+    {
+        _tracer(message);
+    }
+}
---
+++ src/EventLogExpert/Utils.cs
@@ -0,0 +1,34 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+using System.Diagnostics;
+
+namespace EventLogExpert;
+
+internal class Utils
+{
+    private static readonly long _maxLogSize = 10 * 1024 * 1024;
+
+    internal static void InitTracing()
+    {
+        // Set up tracing to a file
+        var eventLogExpertDataFolder = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "EventLogExpert");
+        var debugLogPath = Path.Join(eventLogExpertDataFolder, "debug.log");
+        var fileInfo = new FileInfo(debugLogPath);
+        if (fileInfo.Exists && fileInfo.Length > _maxLogSize)
+        {
+            fileInfo.Delete();
+        }
+
+        System.Diagnostics.Trace.Listeners.Add(new TextWriterTraceListener(debugLogPath, "myListener"));
+        System.Diagnostics.Trace.AutoFlush = true;
+
+        // Trace all exceptions
+        AppDomain.CurrentDomain.FirstChanceException += (o, args) =>
+        {
+           Trace($"{args.Exception}");
+        };
+    }
+
+    internal static void Trace(string message) => System.Diagnostics.Trace.WriteLine($"{DateTime.Now:o} {message}");
+}
