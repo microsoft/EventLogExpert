00019410cf84e17d0f738458710cd6ea9b6f8e96
Joseph Schick <jschick@microsoft.com>
2023-04-21 19:28:50 -0500

    Updated JS mouse events and CSS to hopefully be more consistent with resizing columns

--- src/EventLogExpert/Components/EventTable.razor.js
+++ src/EventLogExpert/Components/EventTable.razor.js
@@ -1,13 +1,61 @@
-﻿export function startColumnResize(dotNetObj) {
-    document.onmousemove = e => {
-        // For some reason we have to manually pass this as a new object. Simply passing the event
-        // to .NET does not work. We're only including the one value we care about.
-        dotNetObj.invokeMethodAsync('MouseMoveCallback', { ClientX: e.clientX });
-    };
+﻿export function enableColumnResize(table) {
+    const columns = table.querySelectorAll('th');
 
-    document.onmouseup = e => {
-        document.onmousemove = null;
-        document.onmouseup = null;
-        dotNetObj.invokeMethodAsync('MouseUpCallback');
-    };
+    if (columns != null) {
+
+        const calculateTableHeight = function () {
+            let height = 0;
+            const tableRows = table.querySelectorAll('tr');
+
+            tableRows.forEach(x => {
+                let firstColumn = x.querySelector('th:first-child,td:first-child');
+                if (firstColumn != null) {
+                    height += firstColumn.offsetHeight;
+                }
+            });
+
+            return height;
+        }
+
+        let actualHeight = calculateTableHeight();
+
+        const createResizableColumn = function (column) {
+            let x = 0;
+            let w = 0;
+
+            const divider = document.createElement('div');
+            divider.classList.add('table-divider');
+
+            divider.style.height = `${actualHeight}px`;
+
+            column.appendChild(divider);
+
+            const mouseDownHandler = function (e) {
+                x = e.clientX;
+
+                const styles = window.getComputedStyle(column);
+                w = parseInt(styles.width, 10);
+
+                document.addEventListener('mousemove', mouseMoveHandler);
+                document.addEventListener('mouseup', mouseUpHandler);
+            };
+
+            const mouseMoveHandler = function (e) {
+                const distance = e.clientX - x;
+
+                column.style.width = `${w + distance}px`;
+            };
+
+            const mouseUpHandler = function () {
+                document.removeEventListener('mousemove', mouseMoveHandler);
+                document.removeEventListener('mouseup', mouseUpHandler);
+            };
+
+            divider.addEventListener('mousedown', mouseDownHandler);
+        };
+
+        [].forEach.call(columns, function (column) {
+            createResizableColumn(column);
+        });
+    }
 }
