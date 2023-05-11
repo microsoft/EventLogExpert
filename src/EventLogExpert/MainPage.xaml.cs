f1b12eaba4cb674ebe3a8e00365b5b58fadeef9f
jschick04 <jschick04@gmail.com>
2023-05-11 15:18:54 -0500

    Fixed DI for fluxor dispatcher to remove threading loop

--- src/EventLogExpert/MainPage.xaml.cs
+++ src/EventLogExpert/MainPage.xaml.cs
@@ -8,28 +8,13 @@ namespace EventLogExpert;
 
 public partial class MainPage : ContentPage
 {
-    private IDispatcher? _fluxorDispatcher;
+    private readonly IDispatcher _fluxorDispatcher;
 
-    public MainPage()
+    public MainPage(IDispatcher fluxorDispatcher)
     {
         InitializeComponent();
 
-        // this.Handler seems to be null even after InitializeComponent completes,
-        // which means we can't access the IOC container to resolve the IDispatcher.
-        // So here we launch a thread that just keeps trying to get the IDispatcher
-        // until it does.
-        Task.Run(() =>
-        {
-            while (_fluxorDispatcher == null)
-            {
-                _fluxorDispatcher = Handler?.MauiContext?.Services.GetService<IDispatcher>();
-
-                if (_fluxorDispatcher == null)
-                {
-                    Thread.Sleep(50);
-                }
-            }
-        });
+        _fluxorDispatcher = fluxorDispatcher;
     }
 
     public async void OpenFile_Clicked(object sender, EventArgs e)
@@ -37,10 +22,7 @@ public partial class MainPage : ContentPage
         var options = new PickOptions
         {
             FileTypes = new FilePickerFileType(
-                new Dictionary<DevicePlatform, IEnumerable<string>>
-                {
-                    { DevicePlatform.WinUI, new[] { ".evtx" } }
-                }
+                new Dictionary<DevicePlatform, IEnumerable<string>> { { DevicePlatform.WinUI, new[] { ".evtx" } } }
             )
         };
 
@@ -48,7 +30,7 @@ public partial class MainPage : ContentPage
 
         if (result != null)
         {
-            _fluxorDispatcher?.Dispatch(
+            _fluxorDispatcher.Dispatch(
                 new EventLogAction.OpenLog(
                     new EventLogState.LogSpecifier(
                         result.FullPath,
@@ -58,7 +40,7 @@ public partial class MainPage : ContentPage
 
     private void OpenLiveLog_Clicked(object sender, EventArgs e)
     {
-        _fluxorDispatcher?.Dispatch(
+        _fluxorDispatcher.Dispatch(
             new EventLogAction.OpenLog(
                 new EventLogState.LogSpecifier(
                     "Application",
