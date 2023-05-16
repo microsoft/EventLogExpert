8f0294442a37c03ef98b9676c6e46adc3ca5bc9b
jschick04 <jschick04@gmail.com>
2023-05-16 10:04:57 -0500

    Implemented update checker during app startup

---
+++ src/EventLogExpert.Library/Models/GitReleaseModel.cs
@@ -0,0 +1,20 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+using System.Text.Json.Serialization;
+
+namespace EventLogExpert.Library.Models;
+
+public record GitReleaseModel
+{
+    [JsonPropertyName("name")] public string Version { get; set; } = null!;
+
+    [JsonPropertyName("assets")] public List<GitReleaseAsset> Assets { get; set; } = null!;
+}
+
+public record GitReleaseAsset
+{
+    [JsonPropertyName("name")] public string Name { get; set; } = null!;
+
+    [JsonPropertyName("browser_download_url")] public string Uri { get; set; } = null!;
+}
