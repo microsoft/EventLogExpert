16132fec4acca0c7ce5e288bc3b33ea49bdeb0b0
jschick04 <jschick04@gmail.com>
2023-05-22 22:13:01 -0500

    Added UI elements for filtering by date

---
+++ src/EventLogExpert.Library/Models/FilterDateModel.cs
@@ -0,0 +1,11 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+namespace EventLogExpert.Library.Models;
+
+public record FilterDateModel
+{
+    public DateTime Before { get; set; }
+
+    public DateTime After { get; set; }
+}
--- src/EventLogExpert/Components/FilterPane.razor.cs
+++ src/EventLogExpert/Components/FilterPane.razor.cs
@@ -1,32 +1,28 @@
 // // Copyright (c) Microsoft Corporation.
 // // Licensed under the MIT License.
 
+using EventLogExpert.Library.Models;
 using EventLogExpert.Store.FilterPane;
 
 namespace EventLogExpert.Components;
 
 public partial class FilterPane
 {
+    private readonly FilterDateModel _model = new();
+
     private bool _expandMenu;
 
-    private string MenuState
-    {
-        get
-        {
-            if (!FilterPaneState.Value.CurrentFilters.Any())
-            {
-                _expandMenu = false;
-            }
-
-            return _expandMenu.ToString().ToLower();
-        }
-    }
+    private string MenuState => _expandMenu.ToString().ToLower();
 
     private void AddFilter()
     {
         Dispatcher.Dispatch(new FilterPaneAction.AddFilter());
         _expandMenu = true;
     }
+
+    private void ApplyDateFilter() { }
+
+    private void ResetDateFilter() { }
 
     private void ToggleMenu() => _expandMenu = !_expandMenu;
 }
