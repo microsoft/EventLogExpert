d647366cbeadfb3bba37b38c2b76457a4f00c84c
jschick04 <jschick04@gmail.com>
2023-05-24 12:19:21 -0500

    Moved Date Filter into collapsable filter list, renamed a few variables to be clearer about intent

--- src/EventLogExpert/Components/FilterPane.razor.cs
+++ src/EventLogExpert/Components/FilterPane.razor.cs
@@ -8,51 +8,67 @@ namespace EventLogExpert.Components;
 
 public partial class FilterPane
 {
-    private FilterDateModel _model = new();
+    private readonly FilterDateModel _model = new();
 
-    private bool _editingDateRange = false;
+    private bool _canEditDate = true;
+    private bool _isDateFilterVisible;
+    private bool _isFilterListVisible;
 
-    private bool _expandMenu;
-
-    private string MenuState => _expandMenu.ToString().ToLower();
-
-    private void AddFilter()
+    private string MenuState
     {
-        Dispatcher.Dispatch(new FilterPaneAction.AddFilter());
-        _expandMenu = true;
+        get
+        {
+            if (FilterPaneState.Value.CurrentFilters.Any() || _isDateFilterVisible)
+            {
+                return _isFilterListVisible.ToString().ToLower();
+            }
+
+            return "false";
+        }
     }
 
     private void AddDateFilter()
     {
-        _model.Before = EventLogState.Value.Events.FirstOrDefault()?.TimeCreated.AddMinutes(1).ConvertTimeZone(SettingsState.Value.TimeZone) ?? DateTime.Now;
-        _model.After = EventLogState.Value.Events.LastOrDefault()?.TimeCreated.AddMinutes(-1).ConvertTimeZone(SettingsState.Value.TimeZone) ?? DateTime.Now;
-        _editingDateRange = true;
+        _isFilterListVisible = true;
+        _canEditDate = true;
+
+        // Offset by 1 minute to make sure we don't drop events
+        // since HTML input DateTime does not go lower than minutes
+        _model.Before = EventLogState.Value.Events.FirstOrDefault()?.TimeCreated
+                .AddMinutes(1).ConvertTimeZone(SettingsState.Value.TimeZone) ??
+            DateTime.Now;
+
+        _model.After = EventLogState.Value.Events.LastOrDefault()?.TimeCreated
+                .AddMinutes(-1).ConvertTimeZone(SettingsState.Value.TimeZone) ??
+            DateTime.Now;
+
+        _isDateFilterVisible = true;
+    }
+
+    private void AddFilter()
+    {
+        Dispatcher.Dispatch(new FilterPaneAction.AddFilter());
+        _isFilterListVisible = true;
     }
 
     private void ApplyDateFilter()
     {
         FilterDateModel model = new()
         {
-            After = _model.After.ToUniversalTime(), 
-            Before = _model.Before.ToUniversalTime()
+            After = _model.After.ToUniversalTime(), Before = _model.Before.ToUniversalTime()
         };
 
         Dispatcher.Dispatch(new FilterPaneAction.SetFilterDateRange(model));
-        _editingDateRange = false;
+        _canEditDate = false;
     }
 
     private void RemoveDateFilter()
     {
         Dispatcher.Dispatch(new FilterPaneAction.SetFilterDateRange(null));
-        _editingDateRange = false;
+        _isDateFilterVisible = false;
     }
 
-    private void EditDateFilter()
-    {
-        _editingDateRange = true;
-    }
+    private void EditDateFilter() => _canEditDate = true;
 
-    public bool IsEditingDisabled => _editingDateRange == false;
-
-    private void ToggleMenu() => _expandMenu = !_expandMenu;
+    private void ToggleMenu() => _isFilterListVisible = !_isFilterListVisible;
 }
