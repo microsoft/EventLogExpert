55a829747b6f0596aa1155604c016a73fe39a7a3
jschick04 <jschick04@gmail.com>
2023-05-27 15:39:41 -0500

    Added a tab pane to easily switch between logs when multiple are loaded

---
+++ src/EventLogExpert/Components/SplitLogTabPane.razor.cs
@@ -0,0 +1,10 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+namespace EventLogExpert.Components;
+
+public partial class SplitLogTabPane
+{
+    // Selecting a tab can also update the App Title so its not flooded with multiple paths
+    // Need to detect if Log Name is is path and split to just show file name (title can contain path)
+}
