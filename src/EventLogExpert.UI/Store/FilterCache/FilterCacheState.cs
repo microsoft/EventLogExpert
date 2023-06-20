69b6781adfc935a001e8015da375694269b1293c
Joseph Schick <jschick@microsoft.com>
2023-06-20 11:03:46 -0500

    Added syling and created state class for recent/favorite filters

---
+++ src/EventLogExpert.UI/Store/FilterCache/FilterCacheState.cs
@@ -0,0 +1,15 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+using Fluxor;
+using System.Collections.Immutable;
+
+namespace EventLogExpert.UI.Store.FilterCache;
+
+[FeatureState]
+public class FilterCacheState
+{
+    public ImmutableList<string> FavoriteFilters { get; set; } = ImmutableList<string>.Empty;
+
+    public ImmutableList<string> RecentFilters { get; set; } = ImmutableList<string>.Empty;
+}
