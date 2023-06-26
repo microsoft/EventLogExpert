476de2c312a400a04a16daa398403afaba1b1ba7
jschick04 <jschick04@gmail.com>
2023-06-25 21:47:07 -0500

    Removed Service files and just grouped common services via comment

--- src/EventLogExpert/MauiProgram.cs
+++ src/EventLogExpert/MauiProgram.cs
@@ -1,7 +1,15 @@
 ﻿// // Copyright (c) Microsoft Corporation.
 // // Licensed under the MIT License.
 
+using EventLogExpert.Eventing.EventResolvers;
+using EventLogExpert.Eventing.Helpers;
 using EventLogExpert.Services;
+using EventLogExpert.UI.Interfaces;
+using EventLogExpert.UI.Options;
+using EventLogExpert.UI.Services;
+using EventLogExpert.UI.Store;
+using EventLogExpert.UI.Store.EventLog;
+using Fluxor;
 
 namespace EventLogExpert;
 
@@ -18,11 +26,44 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
             });
 
-        builder.Services.AddCoreServices();
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddBlazorWebViewDeveloperTools();
 
-        builder.Services.AddBuildServices();
-        builder.Services.AddProviderServices();
-        builder.Services.AddUiServices();
+        builder.Services.AddFluxor(options =>
+        {
+            options.ScanAssemblies(typeof(MauiProgram).Assembly).WithLifetime(StoreLifetime.Singleton);
+            options.AddMiddleware<LoggingMiddleware>();
+        });
+
+        // Core Services
+        builder.Services.AddSingleton<ITraceLogger, DebugLogService>();
+        builder.Services.AddSingleton<ILogWatcherService, LiveLogWatcherService>();
+
+        var fileLocationOptions = new FileLocationOptions(FileSystem.AppDataDirectory);
+        builder.Services.AddSingleton(fileLocationOptions);
+        Directory.CreateDirectory(fileLocationOptions.DatabasePath);
+
+        // Build Services
+        builder.Services.AddSingleton<ICurrentVersionProvider, CurrentVersionProvider>();
+        builder.Services.AddSingleton<IUpdateService, UpdateService>();
+        builder.Services.AddSingleton<IGitHubService, GitHubService>();
+        builder.Services.AddSingleton<IDeploymentService, DeploymentService>();
+
+        // Provider Services
+        builder.Services.AddSingleton<IEnabledDatabaseCollectionProvider, EnabledDatabaseCollectionProvider>();
+        builder.Services.AddSingleton<IDatabaseCollectionProvider, EnabledDatabaseCollectionProvider>();
+        builder.Services.AddTransient<IEventResolver, VersatileEventResolver>();
+
+        // UI Services
+        builder.Services.AddSingleton<IMainThreadService>(new MainThreadService(MainThread.InvokeOnMainThreadAsync));
+        builder.Services.AddSingleton<ITitleProvider, TitleProvider>();
+        builder.Services.AddSingleton<IAppTitleService, AppTitleService>();
+        builder.Services.AddSingleton<IPreferencesProvider, PreferencesProvider>();
+
+        builder.Services.AddSingleton<IAlertDialogService>(new AlertDialogService(
+            (title, message, cancel) => Application.Current!.MainPage!.DisplayAlert(title, message, cancel),
+            async (title, message, accept, cancel) =>
+                await Application.Current!.MainPage!.DisplayAlert(title, message, accept, cancel)));
 
         return builder.Build();
     }
