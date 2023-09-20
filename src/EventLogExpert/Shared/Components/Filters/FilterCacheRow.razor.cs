c1cee8db6ee6485486db3057ec1ab0432871d823
jschick04 <jschick04@gmail.com>
2023-09-20 12:07:02 -0500

    Updated cache filter to allow for direct editing

--- src/EventLogExpert/Shared/Components/Filters/FilterCacheRow.razor.cs
+++ src/EventLogExpert/Shared/Components/Filters/FilterCacheRow.razor.cs
@@ -1,12 +1,14 @@
 ﻿// // Copyright (c) Microsoft Corporation.
 // // Licensed under the MIT License.
 
+using EventLogExpert.Eventing.Models;
 using EventLogExpert.UI;
 using EventLogExpert.UI.Models;
 using EventLogExpert.UI.Store.FilterCache;
 using EventLogExpert.UI.Store.FilterPane;
 using Fluxor;
 using Microsoft.AspNetCore.Components;
+using System.Linq.Dynamic.Core;
 using IDispatcher = Fluxor.IDispatcher;
 
 namespace EventLogExpert.Shared.Components.Filters;
@@ -15,7 +17,9 @@ public partial class FilterCacheRow
 {
     private CacheType _cacheType = CacheType.Favorites;
     private FilterCacheModel _filter = null!;
+    private string _filterValue = null!;
     private bool _isEditing;
+    private string? _errorMessage;
 
     [Parameter] public FilterCacheModel Value { get; set; } = null!;
 
@@ -23,10 +27,45 @@ public partial class FilterCacheRow
 
     [Inject] private IState<FilterCacheState> FilterCacheState { get; set; } = null!;
 
+    private List<string> Items => _cacheType switch
+    {
+        CacheType.Favorites => FilterCacheState.Value.FavoriteFilters.Select(x => x.ComparisonString).ToList(),
+        CacheType.Recent => FilterCacheState.Value.RecentFilters.Select(x => x.ComparisonString).ToList(),
+        _ => new List<string>(),
+    };
+
+    protected override void OnInitialized()
+    {
+        _filterValue = Value.ComparisonString;
+
+        base.OnInitialized();
+    }
+
     private void EditFilter()
     {
         _isEditing = true;
-        _filter = Value;
+        _filter = Value with { };
+    }
+
+    private static bool TryParseExpression(string? expression, out string error)
+    {
+        error = string.Empty;
+
+        if (string.IsNullOrEmpty(expression)) { return false; }
+
+        try
+        {
+            var _ = new List<DisplayEventModel>().AsQueryable()
+                .Where(EventLogExpertCustomTypeProvider.ParsingConfig, expression)
+                .ToList();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            error = ex.Message;
+            return false;
+        }
     }
 
     private void RemoveFilter()
@@ -39,12 +78,24 @@ public partial class FilterCacheRow
 
     private void SaveFilter()
     {
+        if (!TryParseExpression(_filterValue, out _errorMessage)) { return; }
+
         _isEditing = false;
 
-        if (ReferenceEquals(Value, _filter)) { return; }
+        if (string.Equals(Value.ComparisonString, _filterValue, StringComparison.OrdinalIgnoreCase)) { return; }
 
-        Dispatcher.Dispatch(new FilterPaneAction.RemoveCachedFilter(_filter));
-        Dispatcher.Dispatch(new FilterPaneAction.AddCachedFilter(Value));
+        try
+        {
+            _filter.ComparisonString = _filterValue;
+
+            Dispatcher.Dispatch(new FilterPaneAction.RemoveCachedFilter(Value));
+            Dispatcher.Dispatch(new FilterCacheAction.AddRecentFilter(_filter));
+            Dispatcher.Dispatch(new FilterPaneAction.AddCachedFilter(_filter));
+        }
+        catch (Exception ex)
+        {
+            _errorMessage = ex.Message;
+        }
     }
 
     private void ToggleFilter() => Dispatcher.Dispatch(new FilterPaneAction.ToggleCachedFilter(Value));
