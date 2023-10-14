ee966c4a8a35014028f085dbb5d8f75e53898129
jschick04 <jschick04@gmail.com>
2023-10-13 20:46:03 -0500

    Implemented basic context menu

---
+++ src/EventLogExpert/Shared/Components/ContextMenu.razor.cs
@@ -0,0 +1,11 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+namespace EventLogExpert.Shared.Components;
+
+public partial class ContextMenu
+{
+    private void CopyEvent() { }
+
+    private void FilterEvent() { }
+}
---
+++ src/EventLogExpert/wwwroot/js/context.js
@@ -0,0 +1,19 @@
+﻿window.invokeContextMenu = (x, y) => {
+    const contextMenu = document.getElementById("context-menu");
+
+    contextMenu.style.position = "fixed";
+    contextMenu.style.top = `${y}px`;
+    contextMenu.style.left = `${x}px`;
+
+    contextMenu.classList.add("active");
+};
+
+window.addEventListener("click", function(e) {
+    const contextMenu = document.getElementById("context-menu");
+
+    contextMenu.style.position = false;
+    contextMenu.style.top = false;
+    contextMenu.style.left = false;
+
+    contextMenu.classList.remove("active")
+});
