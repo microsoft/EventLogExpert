94bd2c7311169e057668882e9c32f9774a6143b5
jschick04 <jschick04@gmail.com>
2023-11-20 09:14:54 -0600

    Cleaned up warnings in UI and main project

--- src/EventLogExpert.UI/Store/Settings/SettingsEffects.cs
+++ src/EventLogExpert.UI/Store/Settings/SettingsEffects.cs
@@ -8,94 +8,93 @@ using Fluxor;
 
 namespace EventLogExpert.UI.Store.Settings;
 
-public class SettingsEffects
+public sealed class SettingsEffects(
+    IPreferencesProvider preferencesProvider,
+    IEnabledDatabaseCollectionProvider enabledDatabaseCollectionProvider,
+    IEventTableColumnProvider eventTableColumnProvider)
 {
-    private readonly IPreferencesProvider _preferencesProvider;
-    private readonly IEnabledDatabaseCollectionProvider _enabledDatabaseCollectionProvider;
-    private readonly IEventTableColumnProvider _eventTableColumnProvider;
-
-    public SettingsEffects(
-        IPreferencesProvider preferencesProvider,
-        IEnabledDatabaseCollectionProvider enabledDatabaseCollectionProvider,
-        IEventTableColumnProvider eventTableColumnProvider)
-    {
-        _preferencesProvider = preferencesProvider;
-        _enabledDatabaseCollectionProvider = enabledDatabaseCollectionProvider;
-        _eventTableColumnProvider = eventTableColumnProvider;
-    }
-
     [EffectMethod(typeof(SettingsAction.LoadColumns))]
-    public async Task HandleLoadColumns(IDispatcher dispatcher)
+    public Task HandleLoadColumns(IDispatcher dispatcher)
     {
-        var columns = _eventTableColumnProvider.GetColumns();
+        var columns = eventTableColumnProvider.GetColumns();
 
         dispatcher.Dispatch(new SettingsAction.LoadColumnsCompleted(columns));
+
+        return Task.CompletedTask;
     }
 
     [EffectMethod(typeof(SettingsAction.LoadDatabases))]
-    public async Task HandleLoadDatabases(IDispatcher dispatcher)
+    public Task HandleLoadDatabases(IDispatcher dispatcher)
     {
-        var databases = _enabledDatabaseCollectionProvider.GetEnabledDatabases();
+        var databases = enabledDatabaseCollectionProvider.GetEnabledDatabases();
 
         dispatcher.Dispatch(new SettingsAction.LoadDatabasesCompleted(databases));
+
+        return Task.CompletedTask;
     }
 
     [EffectMethod(typeof(SettingsAction.LoadSettings))]
-    public async Task HandleLoadSettings(IDispatcher dispatcher)
+    public Task HandleLoadSettings(IDispatcher dispatcher)
     {
         SettingsModel config = new()
         {
-            TimeZoneId = _preferencesProvider.TimeZonePreference,
-            DisabledDatabases = _preferencesProvider.DisabledDatabasesPreference,
-            ShowDisplayPaneOnSelectionChange = _preferencesProvider.DisplayPaneSelectionPreference,
-            LogLevel = _preferencesProvider.LogLevelPreference,
-            IsPrereleaseEnabled = _preferencesProvider.PrereleasePreference
+            TimeZoneId = preferencesProvider.TimeZonePreference,
+            DisabledDatabases = preferencesProvider.DisabledDatabasesPreference,
+            ShowDisplayPaneOnSelectionChange = preferencesProvider.DisplayPaneSelectionPreference,
+            LogLevel = preferencesProvider.LogLevelPreference,
+            IsPrereleaseEnabled = preferencesProvider.PrereleasePreference
         };
 
         dispatcher.Dispatch(new SettingsAction.LoadSettingsCompleted(config));
+
+        return Task.CompletedTask;
     }
 
     [EffectMethod]
-    public async Task HandleSave(SettingsAction.Save action, IDispatcher dispatcher)
+    public Task HandleSave(SettingsAction.Save action, IDispatcher dispatcher)
     {
-        _preferencesProvider.TimeZonePreference = action.Settings.TimeZoneId;
-        _preferencesProvider.DisabledDatabasesPreference = action.Settings.DisabledDatabases;
-        _preferencesProvider.DisplayPaneSelectionPreference = action.Settings.ShowDisplayPaneOnSelectionChange;
-        _preferencesProvider.LogLevelPreference = action.Settings.LogLevel;
-        _preferencesProvider.PrereleasePreference = action.Settings.IsPrereleaseEnabled;
+        preferencesProvider.TimeZonePreference = action.Settings.TimeZoneId;
+        preferencesProvider.DisabledDatabasesPreference = action.Settings.DisabledDatabases;
+        preferencesProvider.DisplayPaneSelectionPreference = action.Settings.ShowDisplayPaneOnSelectionChange;
+        preferencesProvider.LogLevelPreference = action.Settings.LogLevel;
+        preferencesProvider.PrereleasePreference = action.Settings.IsPrereleaseEnabled;
 
         dispatcher.Dispatch(new SettingsAction.SaveCompleted(action.Settings));
+
+        return Task.CompletedTask;
     }
 
     [EffectMethod]
-    public async Task HandleToggleColumn(SettingsAction.ToggleColumn action, IDispatcher dispatcher)
+    public Task HandleToggleColumn(SettingsAction.ToggleColumn action, IDispatcher dispatcher)
     {
         switch (action.ColumnName)
         {
             case ColumnName.Level :
-                _preferencesProvider.LevelColumnPreference = !_preferencesProvider.LevelColumnPreference;
+                preferencesProvider.LevelColumnPreference = !preferencesProvider.LevelColumnPreference;
                 break;
             case ColumnName.DateAndTime :
-                _preferencesProvider.DateAndTimeColumnPreference = !_preferencesProvider.DateAndTimeColumnPreference;
+                preferencesProvider.DateAndTimeColumnPreference = !preferencesProvider.DateAndTimeColumnPreference;
                 break;
             case ColumnName.ActivityId :
-                _preferencesProvider.ActivityIdColumnPreference = !_preferencesProvider.ActivityIdColumnPreference;
+                preferencesProvider.ActivityIdColumnPreference = !preferencesProvider.ActivityIdColumnPreference;
                 break;
             case ColumnName.LogName :
-                _preferencesProvider.LogNameColumnPreference = !_preferencesProvider.LogNameColumnPreference;
+                preferencesProvider.LogNameColumnPreference = !preferencesProvider.LogNameColumnPreference;
                 break;
             case ColumnName.ComputerName :
-                _preferencesProvider.ComputerNameColumnPreference = !_preferencesProvider.ComputerNameColumnPreference;
+                preferencesProvider.ComputerNameColumnPreference = !preferencesProvider.ComputerNameColumnPreference;
                 break;
             case ColumnName.Source :
-                _preferencesProvider.SourceColumnPreference = !_preferencesProvider.SourceColumnPreference;
+                preferencesProvider.SourceColumnPreference = !preferencesProvider.SourceColumnPreference;
                 break;
             case ColumnName.EventId :
-                _preferencesProvider.EventIdColumnPreference = !_preferencesProvider.EventIdColumnPreference;
+                preferencesProvider.EventIdColumnPreference = !preferencesProvider.EventIdColumnPreference;
                 break;
             case ColumnName.TaskCategory :
-                _preferencesProvider.TaskCategoryColumnPreference = !_preferencesProvider.TaskCategoryColumnPreference;
+                preferencesProvider.TaskCategoryColumnPreference = !preferencesProvider.TaskCategoryColumnPreference;
                 break;
         }
+
+        return Task.CompletedTask;
     }
 }
