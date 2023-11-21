ab55c902efbf1481275cfd526a9010665fd0e232
jschick04 <jschick04@gmail.com>
2023-11-21 11:20:10 -0600

    Fixed dropdown closing on focus lost and context/table column menu showing at the same time

--- src/EventLogExpert/wwwroot/js/context.js
+++ src/EventLogExpert/wwwroot/js/context.js
@@ -1,5 +1,31 @@
-﻿window.invokeContextMenu = (x, y) => {
+﻿window.closeContextMenu = () => {
     const contextMenu = document.getElementById("context-menu");
+
+    if (!contextMenu.classList.contains("active")) { return; }
+
+    contextMenu.style.position = false;
+    contextMenu.style.top = false;
+    contextMenu.style.left = false;
+
+    contextMenu.classList.remove("active");
+}
+
+window.closeTableColumnMenu = () => {
+    const tableColumnMenu = document.getElementById("table-column-menu");
+
+    if (!tableColumnMenu.classList.contains("active")) { return; }
+
+    tableColumnMenu.style.position = false;
+    tableColumnMenu.style.top = false;
+    tableColumnMenu.style.left = false;
+
+    tableColumnMenu.classList.remove("active");
+}
+
+window.invokeContextMenu = (x, y) => {
+    const contextMenu = document.getElementById("context-menu");
+
+    closeTableColumnMenu();
 
     contextMenu.style.position = "fixed";
     contextMenu.style.top = `${y}px`;
@@ -11,6 +37,8 @@
 window.invokeTableColumnMenu = (x, y) => {
     const tableColumnMenu = document.getElementById("table-column-menu");
 
+    closeContextMenu();
+
     tableColumnMenu.style.position = "fixed";
     tableColumnMenu.style.top = `${y}px`;
     tableColumnMenu.style.left = `${x}px`;
@@ -19,25 +47,6 @@ window.invokeTableColumnMenu = (x, y) => {
 };
 
 window.addEventListener("click", function(e) {
-    const contextMenu = document.getElementById("context-menu");
-
-    if (!contextMenu.classList.contains("active")) { return; }
-
-    contextMenu.style.position = false;
-    contextMenu.style.top = false;
-    contextMenu.style.left = false;
-
-    contextMenu.classList.remove("active");
-});
-
-window.addEventListener("click", function(e) {
-    const tableColumnMenu = document.getElementById("table-column-menu");
-
-    if (!tableColumnMenu.classList.contains("active")) { return; }
-
-    tableColumnMenu.style.position = false;
-    tableColumnMenu.style.top = false;
-    tableColumnMenu.style.left = false;
-
-    tableColumnMenu.classList.remove("active");
+    closeContextMenu();
+    closeTableColumnMenu();    
 });
