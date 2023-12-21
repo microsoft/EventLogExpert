5d635e3f1fb35885f48984f390ff7217b87be4ff
jschick04 <jschick04@gmail.com>
2023-12-21 12:19:30 -0600

    Added simple tests for varios FilterModels

--- src/EventLogExpert.UI.Tests/Models/AdvancedFilterModelTests.cs
+++ src/EventLogExpert.UI.Tests/Models/AdvancedFilterModelTests.cs
@@ -1,3 +1,23 @@
-﻿namespace EventLogExpert.UI.Tests.Models;
+using EventLogExpert.UI.Models;
+using System.Linq.Dynamic.Core.Exceptions;
 
-public sealed class AdvancedFilterModelTests { }
+namespace EventLogExpert.UI.Tests.Models;
+
+public sealed class AdvancedFilterModelTests
+{
+    [Fact]
+    public void Comparison_WhenValid_ShouldContainFunc()
+    {
+        AdvancedFilterModel model = new() { ComparisonString = "Id == 100" };
+
+        Assert.NotNull(model.Comparison);
+    }
+
+    [Fact]
+    public void ComparisonString_WhenNotValid_ShouldThrow()
+    {
+        AdvancedFilterModel model = new();
+
+        Assert.Throws<ParseException>(() => model.ComparisonString = "Id == invalid");
+    }
+}
--- src/EventLogExpert.UI.Tests/Models/FilterModelTests.cs
+++ src/EventLogExpert.UI.Tests/Models/FilterModelTests.cs
@@ -1,6 +1,43 @@
-﻿namespace EventLogExpert.UI.Tests.Models;
+﻿using System.Linq.Dynamic.Core.Exceptions;
+using EventLogExpert.UI.Models;
+
+namespace EventLogExpert.UI.Unit.Models;
 
 public sealed class FilterModelTests
 {
-    public void ComparisonString_WhenNotValid_ShouldThrow() { }
+    [Fact]
+    public void Comparison_WhenValid_ShouldContainFunc()
+    {
+        FilterModel model = new() { ComparisonString = "Id == 100" };
+
+        Assert.NotNull(model.Comparison);
+    }
+
+    [Fact]
+    public void ComparisonString_WhenNotValid_ShouldThrow()
+    {
+        FilterModel model = new();
+
+        Assert.Throws<ParseException>(() => model.ComparisonString = "Id == invalid");
+    }
+
+    [Fact]
+    public void FilterValue_WhenFilterTypeChanged_IsNull()
+    {
+        FilterModel model = new() { FilterType = FilterType.Id, FilterValue = "100" };
+
+        model.FilterType = FilterType.Level;
+
+        Assert.Null(model.FilterValue);
+    }
+
+    [Fact]
+    public void FilterValues_WhenFilterTypeChanged_IsEmpty()
+    {
+        FilterModel model = new() { FilterType = FilterType.Id, FilterValues = ["100", "1000"] };
+
+        model.FilterType = FilterType.Level;
+
+        Assert.Empty(model.FilterValues);
+    }
 }
