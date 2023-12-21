faf4bee572cbf812464968f2e834b27bfa33e534
jschick04 <jschick04@gmail.com>
2023-12-21 13:10:13 -0600

    Added tests for AppTitleService

--- src/EventLogExpert.UI.Tests/Services/AppTitleServiceTests.cs
+++ src/EventLogExpert.UI.Tests/Services/AppTitleServiceTests.cs
@@ -1,3 +1,83 @@
-﻿namespace EventLogExpert.UI.Tests.Services;
+﻿using EventLogExpert.UI.Interfaces;
+using EventLogExpert.UI.Services;
+using EventLogExpert.UI.Tests.TestUtils.Constants;
+using NSubstitute;
 
-public sealed class AppTitleServiceTests { }
+namespace EventLogExpert.UI.Tests.Services;
+
+public sealed class AppTitleServiceTests
+{
+    private readonly ICurrentVersionProvider _mockCurrentVersionProvider = Substitute.For<ICurrentVersionProvider>();
+    private readonly ITitleProvider _mockTitleProvider = Substitute.For<ITitleProvider>();
+
+    public AppTitleServiceTests()
+    {
+        _mockCurrentVersionProvider.CurrentVersion.Returns(new Version(Constants.AppInstalledVersion));
+    }
+
+    [Fact]
+    public void SetIsPrerelease_WhenLogName_ShouldSetTitle()
+    {
+        AppTitleService titleService = new(_mockCurrentVersionProvider, _mockTitleProvider);
+
+        titleService.SetIsPrerelease(true);
+        titleService.SetLogName(Constants.LogName);
+
+        _mockTitleProvider.Received(1)
+            .SetTitle($"{Constants.LogName} - {Constants.AppName} (Preview) {Constants.AppInstalledVersion}");
+    }
+
+    [Fact]
+    public void SetLogName_WhenDevBuild_ShouldSetTitle()
+    {
+        AppTitleService titleService = new(_mockCurrentVersionProvider, _mockTitleProvider);
+
+        _mockCurrentVersionProvider.IsDevBuild.Returns(true);
+
+        titleService.SetLogName(null);
+
+        _mockTitleProvider.Received(1).SetTitle($"{Constants.AppName} (Development)");
+    }
+
+    [Fact]
+    public void SetLogName_WhenLogName_ShouldSetTitle()
+    {
+        AppTitleService titleService = new(_mockCurrentVersionProvider, _mockTitleProvider);
+
+        titleService.SetLogName(Constants.LogName);
+
+        _mockTitleProvider.Received(1)
+            .SetTitle($"{Constants.LogName} - {Constants.AppName} {Constants.AppInstalledVersion}");
+    }
+
+    [Fact]
+    public void SetLogName_WhenNullLogName_ShouldSetTitle()
+    {
+        AppTitleService titleService = new(_mockCurrentVersionProvider, _mockTitleProvider);
+
+        titleService.SetLogName(null);
+
+        _mockTitleProvider.Received(1).SetTitle($"{Constants.AppName} {Constants.AppInstalledVersion}");
+    }
+
+    [Fact]
+    public void SetProgressString_WhenNullProgress_ShouldSetTitle()
+    {
+        AppTitleService titleService = new(_mockCurrentVersionProvider, _mockTitleProvider);
+
+        titleService.SetProgressString(null);
+
+        _mockTitleProvider.Received(1).SetTitle($"{Constants.AppName} {Constants.AppInstalledVersion}");
+    }
+
+    [Fact]
+    public void SetProgressString_WhenProgress_ShouldSetTitle()
+    {
+        AppTitleService titleService = new(_mockCurrentVersionProvider, _mockTitleProvider);
+
+        titleService.SetProgressString(Constants.Percentage);
+
+        _mockTitleProvider.Received(1)
+            .SetTitle($"{Constants.Percentage} - {Constants.AppName} {Constants.AppInstalledVersion}");
+    }
+}
