5fcaa875a0ad588e4eb4baf6ada91522b027f115
jschick04 <jschick04@gmail.com>
2024-03-01 10:14:22 -0600

    Multi select will no longer close dropdown when selecting values

--- src/EventLogExpert/wwwroot/js/dropdowns.js
+++ src/EventLogExpert/wwwroot/js/dropdowns.js
@@ -1,31 +1,46 @@
 window.registerDropdown = (root) => {
+    const dropdown = root.getElementsByClassName("dropdown-list")[0];
     const input = root.getElementsByTagName("input")[0];
 
-    if (input) {
-        input.addEventListener("blur",
-            (e) => {
-                const target = e.currentTarget.parentNode;
+    const closeDropdown = (e) => {
+        const target = e.currentTarget.parentNode;
 
-                requestAnimationFrame(() => {
-                    if (target.contains(document.activeElement)) { return; }
+        requestAnimationFrame(() => {
+            if (target.contains(document.activeElement)) { return; }
 
-                    closeDropdown(root);
-                });
-            });
-    }
-
-    root.addEventListener("blur",
-        (e) => {
-            const target = e.currentTarget;
-
-            requestAnimationFrame(() => {
-                if (target.contains(document.activeElement)) { return; }
-
-                closeDropdown(root);
-            });
+            dropdown.removeAttribute("data-toggle");
         });
+    };
 
-    root.addEventListener("click", (e) => toggleDropdown(root));
+    const scrollToSelectedItem = () => {
+        const items = dropdown.getElementsByTagName("div");
+
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].getAttribute("selected") !== null) {
+                items[i].scrollIntoView({ block: "nearest" });
+
+                return;
+            }
+        }
+    };
+
+    const toggleDropdown = () => {
+        const bounds = root.getBoundingClientRect();
+
+        dropdown.style.position = "fixed";
+        dropdown.style.top = `${bounds.bottom + 4}px`;
+        dropdown.style.left = `${bounds.left}px`;
+        dropdown.style.width = `${bounds.width}px`;
+
+        if (dropdown.toggleAttribute("data-toggle")) {
+            scrollToSelectedItem();
+        }
+    };
+
+    input.addEventListener("click", (e) => toggleDropdown(root));
+    input.addEventListener("blur", (e) => closeDropdown(e));
+
+    dropdown.addEventListener("blur", (e) => closeDropdown(e));
 };
 
 window.closeDropdown = (root) => {
