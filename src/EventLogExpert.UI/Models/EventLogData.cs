868b59808be47b7857802dbfc97cea19b68f896d
jschick04 <jschick04@gmail.com>
2024-10-21 13:22:23 -0500

    Removed redundant ordering when getting category values

--- src/EventLogExpert.UI/Models/EventLogData.cs
+++ src/EventLogExpert.UI/Models/EventLogData.cs
@@ -14,27 +14,23 @@ public readonly record struct EventLogData(
 {
     public EventLogId Id { get; } = EventLogId.Create();
 
+    /// <summary>Gets a distinct list of values for the specified category.</summary>
     public IEnumerable<string> GetCategoryValues(FilterCategory category)
     {
         switch (category)
         {
             case FilterCategory.Id:
-                return Events.Select(e => e.Id.ToString())
-                    .Distinct().Order();
+                return Events.Select(e => e.Id.ToString()).Distinct();
             case FilterCategory.ActivityId:
-                return Events.Select(e => e.ActivityId?.ToString() ?? string.Empty)
-                    .Distinct().Order();
+                return Events.Select(e => e.ActivityId?.ToString() ?? string.Empty).Distinct();
             case FilterCategory.Level:
                 return Enum.GetNames<SeverityLevel>();
             case FilterCategory.KeywordsDisplayNames:
-                return Events.SelectMany(e => e.KeywordsDisplayNames)
-                    .Distinct().Order();
+                return Events.SelectMany(e => e.KeywordsDisplayNames).Distinct();
             case FilterCategory.Source:
-                return Events.Select(e => e.Source)
-                    .Distinct().Order();
+                return Events.Select(e => e.Source).Distinct();
             case FilterCategory.TaskCategory:
-                return Events.Select(e => e.TaskCategory)
-                    .Distinct().Order();
+                return Events.Select(e => e.TaskCategory).Distinct();
             case FilterCategory.Xml:
             case FilterCategory.Description:
             default:
