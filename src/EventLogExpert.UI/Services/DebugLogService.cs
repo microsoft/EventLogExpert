f399798a1c1e7bd33ad39350772e1e3118048a7f
jschick04 <jschick04@gmail.com>
2024-11-27 10:54:57 -0600

    Added help menu option to view debug log file

--- src/EventLogExpert.UI/Services/DebugLogService.cs
+++ src/EventLogExpert.UI/Services/DebugLogService.cs
@@ -2,25 +2,83 @@
 // // Licensed under the MIT License.
 
 using EventLogExpert.Eventing.Helpers;
-using EventLogExpert.UI.Interfaces;
 using EventLogExpert.UI.Options;
+using EventLogExpert.UI.Store.Settings;
+using Fluxor;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace EventLogExpert.UI.Services;
 
-public class DebugLogService : ITraceLogger
+public sealed class DebugLogService : ITraceLogger
 {
-    private static readonly long _maxLogSize = 10 * 1024 * 1024;
+    private const long MaxLogSize = 10 * 1024 * 1024;
+    private static readonly ReaderWriterLockSlim s_loggingFileLock = new();
 
     private readonly FileLocationOptions _fileLocationOptions;
-    private readonly LogLevel _loggingLevel;
+    private readonly IState<SettingsState> _settingsState;
 
-    public DebugLogService(FileLocationOptions fileLocationOptions, IPreferencesProvider preferencesProvider)
+    public DebugLogService(FileLocationOptions fileLocationOptions, IState<SettingsState> settingsState)
     {
-        _loggingLevel = preferencesProvider.LogLevelPreference;
         _fileLocationOptions = fileLocationOptions;
+        _settingsState = settingsState;
+
         InitTracing();
+    }
+
+    public async Task ClearAsync()
+    {
+        s_loggingFileLock.EnterWriteLock();
+
+        try
+        {
+            await File.WriteAllTextAsync(_fileLocationOptions.LoggingPath, string.Empty);
+        }
+        finally
+        {
+            s_loggingFileLock.ExitWriteLock();
+        }
+    }
+
+    public async IAsyncEnumerable<string> LoadAsync()
+    {
+        s_loggingFileLock.EnterReadLock();
+
+        try
+        {
+            await foreach (var line in File.ReadLinesAsync(_fileLocationOptions.LoggingPath))
+            {
+                yield return line;
+            }
+        }
+        finally
+        {
+            s_loggingFileLock.ExitReadLock();
+        }
+    }
+
+    public void Trace(string message, LogLevel level = LogLevel.Information)
+    {
+        if (level < _settingsState.Value.Config.LogLevel) { return; }
+
+        string output = $"[{DateTime.Now:o}] [{Environment.CurrentManagedThreadId}] [{level}] {message}";
+
+        s_loggingFileLock.EnterWriteLock();
+
+        try
+        {
+            using StreamWriter writer = File.AppendText(_fileLocationOptions.LoggingPath);
+
+            writer.WriteLine(output);
+        }
+        finally
+        {
+            s_loggingFileLock.ExitWriteLock();
+        }
+
+#if DEBUG
+        Debug.WriteLine(output);
+#endif
     }
 
     private void InitTracing()
@@ -28,18 +86,15 @@ public class DebugLogService : ITraceLogger
         // Set up tracing to a file
         var fileInfo = new FileInfo(_fileLocationOptions.LoggingPath);
 
-        if (fileInfo.Exists && fileInfo.Length > _maxLogSize)
+        if (fileInfo is { Exists: true, Length: > MaxLogSize })
         {
             fileInfo.Delete();
         }
 
-        System.Diagnostics.Trace.Listeners.Add(new TextWriterTraceListener(_fileLocationOptions.LoggingPath, "myListener"));
-        System.Diagnostics.Trace.AutoFlush = true;
-
         // Disabling first chance exception logging unless LogLevel is at Trace level
         // since it is noisy and is logging double information for exceptions that are being handled.
         // This also saves any potential performance hit for when we aren't worried about tracking first chance exceptions.
-        if (_loggingLevel > LogLevel.Trace) { return; }
+        if (_settingsState.Value.Config.LogLevel > LogLevel.Trace) { return; }
 
         var firstChanceSemaphore = new SemaphoreSlim(1);
 
@@ -58,12 +113,5 @@ public class DebugLogService : ITraceLogger
                 firstChanceSemaphore.Release();
             }
         };
-    }
-
-    public void Trace(string message, LogLevel level = LogLevel.Information)
-    {
-        if (level < _loggingLevel) { return; }
-
-        System.Diagnostics.Trace.WriteLine($"[{DateTime.Now:o}] [{Environment.CurrentManagedThreadId}] [{level}] {message}");
     }
 }
