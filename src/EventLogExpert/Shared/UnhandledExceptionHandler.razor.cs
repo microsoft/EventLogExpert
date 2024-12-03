3e892b83e00024afd4dc4bd36122bb30bed863cf
jschick04 <jschick04@gmail.com>
2024-12-03 17:48:45 -0600

    Updated unhandled exception handler in UI

---
+++ src/EventLogExpert/Shared/UnhandledExceptionHandler.razor.cs
@@ -0,0 +1,21 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+using EventLogExpert.Eventing.Helpers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Logging;
+
+namespace EventLogExpert.Shared;
+
+public partial class UnhandledExceptionHandler : ErrorBoundary
+{
+    [Inject] private ITraceLogger TraceLogger { get; set; } = null!;
+
+    protected override Task OnErrorAsync(Exception exception)
+    {
+        TraceLogger.Trace($"Unhandled exception in UI:\r\n{exception}", LogLevel.Critical);
+
+        return base.OnErrorAsync(exception);
+    }
+}
