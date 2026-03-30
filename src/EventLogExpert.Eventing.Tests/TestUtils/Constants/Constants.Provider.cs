59dbe83e402bb07aa8f188479c84f9d5b772dcf9
jschick04 <jschick04@gmail.com>
2026-03-30 18:18:11 -0500

    Added tests for event message provider

---
+++ src/EventLogExpert.Eventing.Tests/TestUtils/Constants/Constants.Provider.cs
@@ -0,0 +1,17 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+namespace EventLogExpert.Eventing.Tests.TestUtils.Constants;
+
+public sealed class Constants
+{
+    public const string NonExistentDll = "NonExistent.dll";
+    public const string NonExistentDllFullPath = @"C:\Windows\System32\NonExistent.dll";
+    public const string NonExistentDllSystemRootFullPath = @"%SystemRoot%\System32\NonExistent.dll";
+
+    public const string LocalComputer = "LocalComputer";
+    public const string RemoteComputer = "RemoteComputer";
+
+    public const string TestProviderLongName = "Microsoft-Windows-EventLogExpert";
+    public const string TestProviderName = "EventLogExpert";
+}
