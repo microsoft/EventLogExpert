e2c1a033bb303a499c44c2b313e95f688abdb819
jschick04 <jschick04@gmail.com>
2026-04-02 18:24:06 -0500

    Added tests for local provider resolver

---
+++ src/EventLogExpert.Eventing.Tests/TestUtils/EventUtils.cs
@@ -0,0 +1,43 @@
+﻿// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+using EventLogExpert.Eventing.Models;
+using static EventLogExpert.Eventing.Tests.TestUtils.Constants.Constants;
+
+namespace EventLogExpert.Eventing.Tests.TestUtils;
+
+public static class EventUtils
+{
+    public static EventRecord CreateBasicEvent() =>
+        new()
+        {
+            RecordId = 1,
+            ProviderName = TestProviderName,
+            Id = 1000,
+            ComputerName = LocalComputer,
+            LogName = ApplicationLogName,
+            TimeCreated = DateTime.UtcNow
+        };
+
+    public static IEnumerable<EventRecord> CreateDifferentEvents() =>
+    [
+        new()
+        {
+            RecordId = 1,
+            ProviderName = TestProviderName,
+            Id = 1000,
+            ComputerName = LocalComputer,
+            LogName = ApplicationLogName,
+            TimeCreated = DateTime.UtcNow
+        },
+        new()
+        {
+            RecordId = 1,
+            ProviderName = TestProviderName,
+            Id = 1001,
+            ComputerName = RemoteComputer,
+            LogName = SystemLogName,
+            TimeCreated = DateTime.UtcNow
+        }
+    ];
+}
