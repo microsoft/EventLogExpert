4f1f0a098135f40c2f0d1e6f6b1bd3a32406a827
jschick04 <jschick04@gmail.com>
2026-05-07 09:22:52 -0500

    Move SeverityLevel and Severity to Models namespace

--- src/EventLogExpert.Eventing/Models/Severity.cs
+++ src/EventLogExpert.Eventing/Models/Severity.cs
@@ -1,17 +1,7 @@
 ﻿// // Copyright (c) Microsoft Corporation.
 // // Licensed under the MIT License.
 
-namespace EventLogExpert.Eventing.Helpers;
-
-/// <summary>Severity levels with values matching ETW standard levels (1–5).</summary>
-public enum SeverityLevel
-{
-    Critical = 1,
-    Error = 2,
-    Warning = 3,
-    Information = 4,
-    Verbose = 5
-}
+namespace EventLogExpert.Eventing.Models;
 
 public static class Severity
 {
