9d254c0404a467c73d60d107596e5f42e29cfe17
jschick04 <jschick04@gmail.com>
2026-05-07 12:19:58 -0500

    Move Eventing.Tests Helpers tests into Interop and Readers

--- src/EventLogExpert.Eventing.Tests/Interop/NativeErrorResolverTests.cs
+++ src/EventLogExpert.Eventing.Tests/Interop/NativeErrorResolverTests.cs
@@ -3,7 +3,7 @@
 
 using EventLogExpert.Eventing.Interop;
 
-namespace EventLogExpert.Eventing.Tests.Helpers;
+namespace EventLogExpert.Eventing.Tests.Interop;
 
 public sealed class NativeErrorResolverTests
 {
