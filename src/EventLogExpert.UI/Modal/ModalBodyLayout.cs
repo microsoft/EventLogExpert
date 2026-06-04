24b99895cf1d415c7066446b93f5a2965143f79c
jschick04 <jschick04@gmail.com>
2026-06-04 10:30:20 -0500

    Extract SidebarTabs<TTab> generic vertical-tablist component from DatabaseToolsModal with WAI-ARIA APG vertical contract (ArrowUp/Down/Home/End; ArrowLeft/Right no longer rotate), add ModalBodyLayout enum with CSS min-height fallback gated on BodyLayout=Flex, and opt DBTools/DebugLog/Settings/ReleaseNotes modals into the new layout contract

---
+++ src/EventLogExpert.UI/Modal/ModalBodyLayout.cs
@@ -0,0 +1,10 @@
+// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
+
+namespace EventLogExpert.UI.Modal;
+
+public enum ModalBodyLayout
+{
+    Content = 0,
+    Flex = 1,
+}
