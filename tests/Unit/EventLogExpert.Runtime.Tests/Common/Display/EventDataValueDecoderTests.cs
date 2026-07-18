302166303f050a466f038b06734c14abf128d3fa
jschick04 <jschick04@gmail.com>
2026-07-18 04:15:47 +0000

    Add curated labels for real update and servicing error codes

--- tests/Unit/EventLogExpert.Runtime.Tests/Common/Display/EventDataValueDecoderTests.cs
+++ tests/Unit/EventLogExpert.Runtime.Tests/Common/Display/EventDataValueDecoderTests.cs
@@ -21,9 +21,11 @@ public sealed class EventDataValueDecoderTests
     [InlineData("errorCode", 0x800F081FL, "CBS_E_SOURCE_MISSING")]
     [InlineData("errorCode", 0x800F0922L, "CBS_E_INSTALLERS_FAILED")]
     [InlineData("errorCode", 0x800F0823L, "CBS_E_NEW_SERVICING_STACK_REQUIRED")]
+    [InlineData("errorCode", 0x800F0816L, "CBS_E_DPX_JOB_STATE_SAVED")]
     [InlineData("errorCode", 0x80073712L, "ERROR_SXS_COMPONENT_STORE_CORRUPT")]
     [InlineData("errorCode", 0x80D05001L, "DO_E_HTTP_BLOCKSIZE_MISMATCH")]
     [InlineData("errorCode", 0x80246007L, "WU_E_DM_NOTDOWNLOADED")]
+    [InlineData("errorCode", 0x8024200BL, "WU_E_UH_INSTALLERFAILURE")]
     public void TryDecodeLabel_KnownCode_ReturnsFriendlyLabel(string fieldName, long code, string expected) =>
         Assert.Equal(expected, EventDataValueDecoder.TryDecodeLabel(fieldName, code));
 
